80d61e8fbd84f6627264e798b89427498e57adeb
Jamie McCullough <jmccullough@utexas.edu>
2023-09-21 16:42:06 +0200

    updated tomographic bin selection effects with real data

--- visualization_scripts/tomo_bin_vis/app.py
+++ visualization_scripts/tomo_bin_vis/app.py
@@ -3,17 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import io
 from math import factorial
+from pathlib import Path
 
 #read in the data to visualize (Generate tomographic bins and apply magnitude cuts)
 cellids = np.arange(150*75).astype(int)
 
-np.random.seed(seed=107)
 binnum=150
-specnum = 200000
-spec_cellids = np.sort(np.random.randint(0,150*75,size=specnum))
-spec_z = np.sort(np.random.normal(loc=0.4,scale=0.4, size=specnum))
-spec_mags_Z = np.random.uniform(low=17.0,high=23.0,size=specnum)
-abund_KV_22 = np.random.randint(0,10000,size=150*75)
+path = Path(__file__).parent
+specs = np.load(path / 'spec_mags.npy')
+spec_cellids = specs[:,1].astype(int)
+spec_z = specs[:,0]
+spec_mags_Z = specs[:,2]
+abund_KV_22 = np.load(path / 'KV_wide_abundances_pt08step.npy')
+magbins = np.arange(15,22,step=0.08)
+
 #get medians everywhere:
 medians = np.array([np.median(spec_z[spec_cellids==i]) for i in cellids])
 
@@ -35,18 +38,21 @@ def savitzky_golay(y, window_size=21, order=4, deriv=0, rate=1):
     # precompute coefficients
     b = np.mat([[k ** i for i in order_range] for k in range(-half_window, half_window + 1)])
     m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
-    # pad the signal at the extremes with
-    # values taken from the signal itself
+
     firstvals = y[0] - np.abs(y[1:half_window + 1][::-1] - y[0])
     lastvals = y[-1] + np.abs(y[-half_window - 1:-1][::-1] - y[-1])
     y = np.concatenate((firstvals, y, lastvals))
     return np.convolve(m[::-1], y, mode='valid')
 
-def get_new_abund_KV(magcut,abund_22= abund_KV_22):
-    #perform a pseudo cut, can read from file as well
-    cut = np.round(np.random.randint(0,high=10000,size=150*75)*magcut/5)
-    cut[cut>abund_22] = abund_22[cut>abund_22] #don't drop below zero, unphysical
-    return abund_22 - cut
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx, array[idx]
+
+def get_new_abund_KV(magcut,magbins=magbins,abund_22= abund_KV_22):
+    id, magcut = find_nearest(magbins,magcut)
+    cut_abund = abund_22[:,id]
+    return cut_abund
 
 def get_masks(spec_cellids,abund_cut,zrange_mask,cellids=cellids):
     #get the cell mask and the spectroscopic mask for a given range of median zs and wide field abundances
@@ -89,12 +95,16 @@ def do_all_calcs_fixed(med,magcut,spec_mag_cut,spec_z=spec_z,spec_mags_Z=spec_ma
     weights_cut = get_weights(spec_cellids[spec_mask],abund_cut)
     weights_orig = get_weights(spec_cellids[spec_mask_orig],abund_cut)
 
-    meanz = np.average(spec_z[spec_mask_orig],weights=weights_orig)
-    meanz_cut = np.average(spec_z[spec_mask],weights=weights_cut)
-    hist_nospecmag, edges = np.histogram(spec_z[spec_mask_orig],bins=bins,weights=weights_orig,density=True)
-    hist_speccut, edges = np.histogram(spec_z[spec_mask],bins=bins,weights=weights_cut,density=True)
+    try:
+        meanz = np.average(spec_z[spec_mask_orig],weights=weights_orig)
+        meanz_cut = np.average(spec_z[spec_mask],weights=weights_cut)
+        hist_nospecmag, edges = np.histogram(spec_z[spec_mask_orig],bins=bins,weights=weights_orig,density=True)
+        hist_speccut, edges = np.histogram(spec_z[spec_mask],bins=bins,weights=weights_cut,density=True)
 
-    return binmids, hist_speccut, hist_nospecmag, meanz_cut, meanz
+        return binmids, hist_speccut, hist_nospecmag, meanz_cut, meanz
+    except:
+        print('no galaxies to infer a redshift for!')
+        return binmids, np.zeros(len(binmids)), np.zeros(len(binmids)), np.NaN,np.NaN
 
 
 #user interface
@@ -104,12 +114,10 @@ app_ui = ui.page_fluid(
         ui.panel_sidebar(
             ui.input_checkbox("smooth","Apply Smoothing",value=True),
             ui.input_numeric("npts","SavGol Smoothing Amount", value=21,step=2),
-            ui.input_slider("med_z", "Median cell redshift selection for bin", min=0, max=1.7, value=[0.3,0.6], step=0.01, width='100%',drag_range=True),  # 2
-            ui.input_slider("magcut", "Wide-field Magnitude Cut, MAG_GAAP_Z", min=15.0, max=22.0, value=22.0, step=0.01, width='100%'),
+            ui.input_slider("med_z", "Median cell redshift selection for tomographic bin", min=0, max=1.7, value=[0.2,0.6], step=0.01, width='100%',drag_range=True),  # 2
+            ui.input_slider("magcut", "Wide-field Magnitude Cut, MAG_GAAP_Z", min=15.0, max=22.0, value=22.0, step=0.08,width='100%'),
             ui.input_slider("spec_mag_cut", "Spectroscopic Magnitude Cut, MAG_GAAP_Z", min=15.0, max=23.0, value=22.0, step=0.01, width='100%'),
             ui.download_button("downloadData", "Download Current Figure"),
-            ui.download_button("downloadall", "Download All Data"),
-
 width=4, height='50px'),
         ui.panel_main(
             ui.output_plot("p",height='650px'),  # 4
@@ -140,13 +148,17 @@ def server(input: Inputs, output: Outputs, session: Session):
 
         #make the figure
         plt.plot(binmids,hist_speccut,'o--',markersize=1.0,color='red',linewidth=2.0,label='spec-$z < {0:.1f}$'.format(input.spec_mag_cut()))
-        plt.plot(binmids,hist_nospecmag,'o-',markersize=1.0,color='black',linewidth=2.0,label='no spec-$z$ cut')
-        plt.axvline(x = meanz_cut,linestyle='dashed',color='red',label=r'mean(z) spec-cut$ = {0:.4f}$'.format(meanz_cut))
-        plt.axvline(x = meanz,linestyle='dashed',color='gray',label=r'mean(z)$ = {0:.4f}$'.format(meanz))
-
-        plt.xlabel(r'Redshift, $z$',fontsize=14)
-        plt.ylabel(r'$N(z)$',fontsize=14)
-        plt.legend(loc='upper right',fontsize=14)
+        plt.plot(binmids,hist_nospecmag,'o-',markersize=1.0,color='black',linewidth=2.0,label='no applied spec-$z$ selection')
+        plt.axvline(x = meanz_cut,linestyle='dashed',color='red',label=r'$\overline{z}_{\mathrm{spec\ cut}} = $'+'{0:.4f}'.format(meanz_cut))
+        plt.axvline(x = meanz,linestyle='dashed',color='gray',label=r'$\overline{z} = $'+'{0:.4f}'.format(meanz))
+        ax = plt.gca()
+        ax.text(0.65,0.85,'$|\Delta \overline{z}| = $'+'{0:.4f}'.format(np.abs(meanz-meanz_cut)),color='gray',
+                fontsize=20,transform=ax.transAxes)
+        ax.text(0.541, 0.9, 'MAG_GAAP_Z Wide < ' + '{0:.2f}'.format(input.magcut()), color='gray',
+                fontsize=20, transform=ax.transAxes)
+        plt.xlabel(r'Redshift, $z$',fontsize=20)
+        plt.ylabel(r'$N(z)$',fontsize=20)
+        plt.legend(loc='upper right',fontsize=18)
         return fig
 
     @session.download(filename='figure.png')
@@ -154,20 +166,34 @@ def server(input: Inputs, output: Outputs, session: Session):
         """
         This is the simplest case returninig bytes, duplicates the plotting function to save
         """
+        fig = plt.figure()
+        fig.set_size_inches(15, 6)
+        binmids, hist_speccut, hist_nospecmag, meanz_cut, meanz = get_data()
 
+        # apply simple smoothing
+        if input.smooth():
+            hist_speccut = savitzky_golay(hist_speccut, window_size=input.npts())
+            hist_nospecmag = savitzky_golay(hist_nospecmag, window_size=input.npts())
+
+        # make the figure
+        plt.plot(binmids, hist_speccut, 'o--', markersize=1.0, color='red', linewidth=2.0,
+                 label='spec-$z < {0:.1f}$'.format(input.spec_mag_cut()))
+        plt.plot(binmids, hist_nospecmag, 'o-', markersize=1.0, color='black', linewidth=2.0,
+                 label='no applied spec-$z$ selection')
+        plt.axvline(x=meanz_cut, linestyle='dashed', color='red',
+                    label=r'$\overline{z}_{\mathrm{spec\ cut}} = $' + '{0:.4f}'.format(meanz_cut))
+        plt.axvline(x=meanz, linestyle='dashed', color='gray', label=r'$\overline{z} = $' + '{0:.4f}'.format(meanz))
+        ax = plt.gca()
+        ax.text(0.587, 0.85, '$|\Delta \overline{z}| = $' + '{0:.4f}'.format(np.abs(meanz - meanz_cut)), color='gray',
+                fontsize=14, transform=ax.transAxes)
+        ax.text(0.4638, 0.9, 'MAG_GAAP_Z Wide < ' + '{0:.2f}'.format(input.magcut()), color='gray',
+                fontsize=14, transform=ax.transAxes)
+        plt.xlabel(r'Redshift, $z$', fontsize=14)
+        plt.ylabel(r'$N(z)$', fontsize=14)
+        plt.legend(loc='upper right', fontsize=14)
 
         with io.BytesIO() as buf:
             plt.savefig(buf, dpi=300,format="png")
             yield buf.getvalue()
-
-
-    @session.download()
-    def downloadAll():
-        """
-        This should return the full data set as a collection of bytes
-        """
-
-        path = ('../4c3r2_messenger_selection.png') #placeholder
-        return path
 
 app = App(app_ui, server)
